a3813273894c966a982b09539deeadde6c6fee3f
yuvraj-sapkota <sharmayuvraj034@gmail.com>
2024-05-03 01:14:49 +0545

    dccn and fps error solved

--- src/components/DCCN.jsx
+++ src/components/DCCN.jsx
@@ -4,158 +4,96 @@ import styled from "./DCCN.module.css";
 function DCCN() {
   const Fps = [
     {
-      img: "./images/dccn1.jpg",
+      img: "./Dccn/Fps1.jpg",
     },
     {
-      img: "./images/dccn2.jpg",
+      img: "./Dccn/Fps2.jpg",
     },
     {
-      img: "./images/dccn3.jpg",
+      img: "./Dccn/Fps3.jpg",
     },
     {
-      img: "./images/dccn4.jpg",
+      img: "./Dccn/Fps4.jpg",
     },
     {
-      img: "./images/dccn5.jpg",
+      img: "./Dccn/Fps5.jpg",
     },
     {
-      img: "./images/dccn6.jpg",
+      img: "./Dccn/Fps6.jpg",
     },
     {
-      img: "./images/dccn7.jpg",
+      img: "./Dccn/Fps7.jpg",
     },
     {
-      img: "./images/dccn8.jpg",
+      img: "./Dccn/Fps8.jpg",
     },
     {
-      img: "./images/dccn9.jpg",
+      img: "./Dccn/Fps9.jpg",
     },
     {
-      img: "./images/dccn10.jpg",
+      img: "./Dccn/Fps10.jpg",
     },
     {
-      img: "./images/dccn11.jpg",
+      img: "./Dccn/Fps11.jpg",
     },
     {
-      img: "./images/dccn12.jpg",
+      img: "./Dccn/Fps12.jpg",
     },
     {
-      img: "./images/dccn13.jpg",
+      img: "./Dccn/Fps13.jpg",
     },
     {
-      img: "./images/dccn14.jpg",
+      img: "./Dccn/Fps14.jpg",
     },
     {
-      img: "./images/dccn15.jpg",
+      img: "./Dccn/Fps15.jpg",
     },
     {
-      img: "./images/dccn16.jpg",
+      img: "./Dccn/Fps16.jpg",
     },
     {
-      img: "./images/dccn17.jpg",
+      img: "./Dccn/Fps17.jpg",
     },
     {
-      img: "./images/dccn18.jpg",
+      img: "./Dccn/Fps18.jpg",
     },
     {
-      img: "./images/dccn19.jpg",
+      img: "./Dccn/Fps19.jpg",
     },
     {
-      img: "./images/dccn20.jpg",
+      img: "./Dccn/Fps20.jpg",
     },
     {
-      img: "./images/dccn21.jpg",
+      img: "./Dccn/Fps21.jpg",
     },
     {
-      img: "./images/dccn22.jpg",
+      img: "./Dccn/Fps22.jpg",
     },
     {
-      img: "./images/dccn23.jpg",
+      img: "./Dccn/Fps23.jpg",
     },
     {
-      img: "./images/dccn24.jpg",
+      img: "./Dccn/Fps24.jpg",
     },
     {
-      img: "./images/dccn25.jpg",
+      img: "./Dccn/Fps25.jpg",
     },
     {
-      img: "./images/dccn26.jpg",
+      img: "./Dccn/Fps26.jpg",
     },
     {
-      img: "./images/dccn27.jpg",
+      img: "./Dccn/Fps27.jpg",
     },
     {
-      img: "./images/dccn28.jpg",
-    },
-    {
-      img: "./images/dccn29.jpg",
-    },
-    {
-      img: "./images/dccn30.jpg",
-    },
-    {
-      img: "./images/dccn31.jpg",
-    },
-    {
-      img: "./images/dccn32.jpg",
-    },
-    {
-      img: "./images/dccn33.jpg",
-    },
-    {
-      img: "./images/dccn34.jpg",
-    },
-    {
-      img: "./images/dccn35.jpg",
-    },
-    {
-      img: "./images/dccn36.jpg",
-    },
-    {
-      img: "./images/dccn37.jpg",
-    },
-    {
-      img: "./images/dccn38.jpg",
-    },
-    {
-      img: "./images/dccn39.jpg",
-    },
-    {
-      img: "./images/dccn40.jpg",
-    },
-    {
-      img: "./images/dccn41.jpg",
-    },
-    {
-      img: "./images/dccn42.jpg",
-    },
-    {
-      img: "./images/dccn43.jpg",
-    },
-    {
-      img: "./images/dccn44.jpg",
-    },
-    {
-      img: "./images/dccn45.jpg",
-    },
-    {
-      img: "./images/dccn46.jpg",
-    },
-    {
-      img: "./images/dccn47.jpg",
-    },
-    {
-      img: "./images/dccn48.jpg",
-    },
-    {
-      img: "./images/dccn49.jpg",
+      img: "./Dccn/Fps28.jpg",
     },
   ];
+
   return (
     <>
       <Header />
       <div className={styled.container}>
-        <p>Fundamental of Probability and Statics</p>
+        <p>Data communication and computer network</p>
         <div className={styled.images}>
           {Fps.map((curElem, index) => {
             const { img } = curElem;
